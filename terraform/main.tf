ce87d5358b749805553ba8a78d1afe5e629f3f4b
IrokuTech <217807622+IrokuTech@users.noreply.github.com>
2026-08-26 19:47:24 +0200

    Add Terraform-managed Nginx container

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,21 @@ provider "docker" {}
 resource "docker_network" "terraform_lab" {
   name = "terraform-lab-network"
 }
+
+resource "docker_image" "nginx" {
+  name = "nginx:alpine"
+}
+
+resource "docker_container" "nginx_lab" {
+  name  = "terraform-nginx-lab"
+  image = docker_image.nginx.image_id
+
+  networks_advanced {
+    name = docker_network.terraform_lab.name
+  }
+
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
